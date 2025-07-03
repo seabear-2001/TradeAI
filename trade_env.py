2333fc61a11876413e72f5b1069ab59d938a5cfa
Chen <1563782247@qq.com>
2025-07-03 22:45:03 +0900

    master

--- trade_env.py
+++ trade_env.py
@@ -105,9 +105,13 @@ class TradeEnv(gymnasium.Env):
 
         # 后续净值更新、回撤、盈亏、止盈止损等逻辑保持不变
         net_worth, old_net_worth, max_net_worth = self.account.update_net_worth(current_price)
-        # reward += (net_worth - old_net_worth) / self.account.initial_balance * 100  # 本步收益
+
+        # if net_worth > self.account.initial_balance:
+        #     reward += (net_worth - old_net_worth) / self.account.initial_balance * 100  # 本步收益
         reward -= ((max_net_worth - net_worth) / max_net_worth) * 100 # 本步回撤  if max_net_worth > 0 else 0
-        # reward -= (net_worth + self.account.initial_balance) / self.account.initial_balance * 20
+        # if net_worth < self.account.initial_balance:
+        #     reward -= self.account.get_gain_ratio() * 100
+
         if not self.live_mode and self.current_step >= len(self.data_array) - 1:
             terminated = True
 
@@ -119,7 +123,7 @@ class TradeEnv(gymnasium.Env):
             'reward': reward
         }
         self.last_print_step = 0
-        if self.current_step - self.last_print_step >= 3000 or terminated:
+        if self.current_step - self.last_print_step >= 10000 or terminated:
             print(info)
             self.last_print_step = self.current_step
 
