077af3d11186588551734e3f58fd0a04eb0444e1
Chen <1563782247@qq.com>
2025-07-04 02:32:52 +0900

    master

--- trade_train_okx_1s.py
+++ trade_train_okx_1s.py
@@ -24,7 +24,7 @@ if system_name == "Windows":
 # 训练数据集划分比例
 TRAIN_RATIO = 0.8
 
-single_step_num = 32 # 每步训练的重复次数
+single_step_num = 20 # 每步训练的重复次数
 eval_freq = 1_000_000
 
 # QRDQN算法相关超参数配置，参考SB3文档和经验调整
@@ -32,7 +32,7 @@ model_kwargs = {
     "learning_rate": 1e-4,            # 学习率，越小越稳定
     "buffer_size": 50_000_000,           # 经验回放池大小，越大越稳定但占内存
     "learning_starts": 20_000,        # 收集多少步后开始训练
-    "batch_size": 4096,                # 每次训练采样大小
+    "batch_size": 64,                # 每次训练采样大小
     "train_freq": 2,                  # 每执行多少步训练一次模型 和 每次训练的更新步数
     "gradient_steps": 1,              # 每次训练的更新步数
     "target_update_interval": 1000,   # 目标网络更新频率
