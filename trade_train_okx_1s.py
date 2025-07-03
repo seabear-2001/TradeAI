24cd247a2c4d4544bb6a7e9a0376beff14f2c944
Chen <1563782247@qq.com>
2025-07-04 02:59:24 +0900

    master

--- trade_train_okx_1s.py
+++ trade_train_okx_1s.py
@@ -32,10 +32,10 @@ model_kwargs = {
     "learning_rate": 1e-4,            # 学习率，越小越稳定
     "buffer_size": 20_000_000,           # 经验回放池大小，越大越稳定但占内存
     "learning_starts": 100_000,        # 收集多少步后开始训练
-    "batch_size": 512,                # 每次训练采样大小
+    "batch_size": 1024,                # 每次训练采样大小
     "train_freq": 2,                  # 每执行多少步训练一次模型 和 每次训练的更新步数
     "gradient_steps": 4,              # 每次训练的更新步数
-    "target_update_interval": 1000,   # 目标网络更新频率
+    "target_update_interval": 2000,   # 目标网络更新频率
     "exploration_fraction": 0.5,      # epsilon衰减比例，前40%训练是探索
     "exploration_final_eps": 0.05,    # epsilon最终最小值
     "gamma": 0.99,                   # 折扣因子，考虑未来奖励的权重
