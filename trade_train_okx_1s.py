e6e07684cd04c049c7c2e64aa9968490e2bc6df5
Chen <1563782247@qq.com>
2025-07-06 21:52:07 +0900

    master

--- trade_train_okx_1s.py
+++ trade_train_okx_1s.py
@@ -38,7 +38,7 @@ model_kwargs = {
     "train_freq": 4,                  # 每执行多少步训练一次模型 和 每次训练的更新步数
     "gradient_steps": 1,              # 每次训练的更新步数
     "target_update_interval": 2000,   # 目标网络更新频率
-    "exploration_fraction": 0.5,      # epsilon衰减比例，前20%训练是探索
+    "exploration_fraction": 0.2,      # epsilon衰减比例，前20%训练是探索
     "exploration_final_eps": 0.02,    # epsilon最终最小值
     "gamma": 0.95,                   # 折扣因子，考虑未来奖励的权重
 }
